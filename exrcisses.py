071a962b04e5784678be42b4f832d7389d87ec59
GherasimStefan <stefangherasim09@gmail.com>
2020-04-30 14:56:51 +0300

    file 2 succesful

---
+++ exrcisses.py
@@ -0,0 +1,21 @@
+a = 1
+b = 2
+
+c = a + b
+print("total is: {}".format(c))
+
+s = "Ana are mere"
+print(s)
+
+print("""acesta este un sir de caractere
+     spart
+   pe mai multe randuri
+""")
+
+print("Introdu un numar")
+
+x = int(input())
+y = 1
+total = x + y
+
+print("x + y este: {}".format(total))
